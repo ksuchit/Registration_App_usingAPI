162e3e310a7be62e730786af9ae3890510daa74f
ksuchit <suchitkore1528@gmail.com>
2022-12-09 00:43:15 +0530

    products page UI

--- src/CompanyDetails/Products/Pagination.js
+++ src/CompanyDetails/Products/Pagination.js
@@ -1,14 +1,36 @@
+import { useState } from "react";
+import { useEffect } from "react"
+import { getProducts } from "../../Services/HttpService";
 
 
 export default function Pagination(props) {
+
+    const[nextPageData,setNextPageData]=useState([]);
+
+    useEffect(()=>{
+        getProducts(`/products?&limit=${props.itemPerPage}&page=${props.pageNum+1}`)
+      .then((response) => {
+        console.log(response);
+        setNextPageData(response.data.results);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    },[])
+
     return (
-        <div>
+        <div className="my-2 d-flex justify-content-center">
             {props.pageNum === 1 ? "" :
-                <button onClick={() => props.setPageNum((prev) => prev - 1)}>Previous</button>
+                <button onClick={() => props.setPageNum((prev) => prev - 1)}
+                    className='btn btn-secondary'                
+                >Previous</button>
             }
-            {props.pageNum}
-            {props.products.length * props.pageNum < props.pageNum * props.itemPerPage ? "" :
-                <button onClick={() => props.setPageNum((prev) => prev + 1)}>Next</button>
+            <p className='fw-bolder mx-2'>{props.pageNum}</p>
+            {nextPageData.length >0 ? 
+                <button onClick={() => props.setPageNum((prev) => prev + 1)}
+                    className='btn btn-secondary'
+                >Next</button>
+            : ""
             }
         </div>
     )
